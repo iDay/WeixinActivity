50ccf3fc573f96b46b254c26c88cabbc57d6e047
iDay <johnny.iday@gmail.com>
2013-12-02 18:17:50 +0800

    Initial Commit

---
+++ WeixinActivity/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeixinActivity
+//
+//  Created by Johnny iDay on 13-12-2.
+//  Copyright (c) 2013年 Johnny iDay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeixinActivity/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WeixinActivity
+//
+//  Created by Johnny iDay on 13-12-2.
+//  Copyright (c) 2013年 Johnny iDay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
